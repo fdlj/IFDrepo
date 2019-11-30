138247c49f6798e86f711b39118dce6b5f9f27ca
fdlj <fdlj@insightersfordecision.co>
2019-12-01 00:50:02 +0100

    mise en prod IFD analytics 12eme tentatives

--- backend/database/dbusers.js
+++ backend/database/dbusers.js
@@ -1,3 +1,7 @@
 module.exports = {
-    db: 'mongodb://localhost:27017/meandb',
+  // en prod
+    db: 'mongodb://127.0.0.1:27017'
+    
+    // en test
+    // db: 'mongodb://localhost:27017/meandb',
   };
